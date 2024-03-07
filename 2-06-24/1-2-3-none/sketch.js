2bff0fff44c3cc6750b3b10858a36fd4836864f4
asulli1 <asulli1@saic.edu>
2024-03-06 20:35:11 -0600

    machine learning stuff

---
+++ 2-06-24/1-2-3-none/sketch.js
@@ -0,0 +1,91 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trained customized model and p5.js
+This example uses p5 preload function to create the classifier
+=== */
+
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'model/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+let myResults = [];
+
+let position;
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
+
+function setup() {
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
+
+  position = createVector (width/2, height/2);
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+ //ellipse(position.x, position.y, 50, 50)
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+
+// const speed = 4
+//   //if 1, move ball up, if 2, ball stays in middle, if 3 ball moves down
+//   if(label == "1" && position.y > 0) {
+//     //position.y = position.y - 1;
+//     position.y-=speed;
+//   } else if (label == "3" && position.y < height) {
+//     position.y+=speed;
+//   } else if (label == "none") {
+//     position.y=position.y
+//   }
+
+
+
+  // Classifiy again!
+  classifyVideo();
+}
